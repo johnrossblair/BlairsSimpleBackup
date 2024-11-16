e6dad02add805dc8aaa123348e46b98ba98c5c09
johnrossblair <146880946+johnrossblair@users.noreply.github.com>
2024-11-15 22:13:52 -0600

    Update simple_backup_tool.sh
    Update 11/15/24:
    - Better spacing between prompts.
    - Added autocomplete with tab to prevent having to type full path manually.
    - Fixed issue where daily backup would run even if full backup was just completed less than 24 hours ago.
    - able to use "~" to specify home directory for ease of use.
    - Now generates a folder within the specified directory labeled "backups" to hold all the folders and files.
    - Auto-adds the backup directory to the exclusion list to prevent backup inception.
    - Generated script is now stored within the backup folder.
    - General cleanup and better logic

--- simple_backup_tool.sh
+++ simple_backup_tool.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clear
 
 echo "##############################################################"
 echo "#   ____    _                   _____   _____    _    _____  #"
@@ -23,29 +24,52 @@ echo "##############################################################"
 echo ""
 
 # Choose backup directory
-read -p "Where would you like to save your backups? (ex: /path/to/backups): " BACKUP_DIR
+read -ep "Where would you like to save your backups? (ex: /path/to/backups): " BACKUP_DIR
 echo ""
+
+    # Clean up path by removing "/" at end if submitted, and also generating full path if user uses ~ shortcut for home
+    if [[ "${BACKUP_DIR:0:1}" == "~" ]]; then
+        BACKUP_DIR="/home/$USER${BACKUP_DIR:1}"
+    fi
+
+    if [[ "${BACKUP_DIR: -1}" == "/" ]]; then
+        BACKUP_DIR="${BACKUP_DIR%/}"
+    fi
 
     # Loop until the entered path exists
     while [ ! -e "$BACKUP_DIR" ]; do
         echo -e "The path '$BACKUP_DIR' does not exist. Please try again. \n"
-        read -p "Where would you like to save your backups? (ex: /path/to/backups): " BACKUP_DIR
+        read -ep "Where would you like to save your backups? (ex: /path/to/backups): " BACKUP_DIR
         echo ""
     done
 
+BACKUP_DIR="${BACKUP_DIR}/backups"
+
+mkdir -p $BACKUP_DIR
+
 # Choose directory to back up
-read -p "What folder would you like to backup? (ex: /home/yourname/Pictures): " BACKUP_TARGET
+read -ep "What folder would you like to backup? (ex: /home/yourname/Pictures): " BACKUP_TARGET
 echo ""
+
+    # Clean up path by removing "/" at end if submitted, and also generating full path if user uses ~ shortcut for home
+    if [[ "${BACKUP_TARGET:0:1}" == "~" ]]; then
+        BACKUP_TARGET="/home/$USER${BACKUP_TARGET:1}"
+    fi
+
+    if [[ "${BACKUP_TARGET: -1}" == "/" ]]; then
+        BACKUP_TARGET="${BACKUP_TARGET%/}"
+    fi
 
     # Loop until the entered path exists
     while [ ! -e "$BACKUP_TARGET" ]; do
         echo -e "The path '$BACKUP_TARGET' does not exist. Please try again. \n"
-        read -p "What folder would you like to backup? (ex: /home/yourname/Pictures): " BACKUP_TARGET
+        read -ep "What folder would you like to backup? (ex: /home/yourname/Pictures): " BACKUP_TARGET
         echo ""
     done
 
 # Check for file/folder exclusion within the directory to be archived
 read -p "Are there any folders or files you would like to exclude within the folder? (y/n): " EXCLUDE_ANSWER
+echo ""
 
 # Path to exclusion list
 EXCLUDE_LIST="$BACKUP_DIR/.exclude_list.txt"
@@ -53,15 +77,30 @@ EXCLUDE_LIST="$BACKUP_DIR/.exclude_list.txt"
 # Ensure list exsists
 touch "$EXCLUDE_LIST"
 
+# add backup directory to avoid backing up a backup - failsafe
+echo "$BACKUP_DIR" > "$EXCLUDE_LIST"
+
 if [ "$EXCLUDE_ANSWER" == "y" ]; then
     # If yes, enter the loop to collect exclusions
     while true; do
         # Ask for a path to exclude
-        read -p "Enter a path to exclude from the backup (or type 'done' to finish): " EXCLUDE_PATH
+        read -ep "Enter a path to exclude from the backup (or type 'done' to finish): " EXCLUDE_PATH
+        echo ""
+
+        # Clean up path by removing "/" at end if submitted, and also generating full path if user uses ~ shortcut for home
+        if [[ "${EXCLUDE_PATH:0:1}" == "~" ]]; then
+            EXCLUDE_PATH="/home/$USER${EXCLUDE_PATH:1}"
+        fi
+
+        if [[ "${EXCLUDE_PATH: -1}" == "/" ]]; then
+            EXCLUDE_PATH="${EXCLUDE_PATH%/}"
+        fi
 
         # Check if the user typed 'done' to exit the loop
         if [ "$EXCLUDE_PATH" == "done" ]; then
-            echo -e "Exclusion list complete. \n"
+            echo "Exclusion list complete. The following paths will not be backed up:"
+            echo "$(cat $EXCLUDE_LIST)"
+            echo ""
             break
         fi
 
@@ -81,7 +120,8 @@ fi
 # Loop for weekly backups
 while true; do
     #Ask how many weekly backups to store
-    read -p "How many weekly backups would you like to keep? (Must be 1 or more): " WEEKLY_LIMIT
+    read -p "How many weekly backups would you like to keep?: " WEEKLY_LIMIT
+    echo ""
     if [[ "$WEEKLY_LIMIT" =~ ^[0-9]+$ ]] && [ "$WEEKLY_LIMIT" -gt 0 ]; then
         echo -e "$WEEKLY_LIMIT weekly backups will be stored at one time. \n"
         break
@@ -93,7 +133,8 @@ done
 # Loop for daily backups
 while true; do
     #Ask how many daily backups to store
-    read -p "How many daily incremental backups would you like to keep? (Must be 1 or more): " DAILY_LIMIT
+    read -p "How many daily incremental backups would you like to keep?: " DAILY_LIMIT
+    echo ""
     if [[ "$DAILY_LIMIT" =~ ^[0-9]+$ ]] && [ "$DAILY_LIMIT" -gt 0 ]; then
         echo -e "$DAILY_LIMIT daily backups will be stored at one time. \n"
         break
@@ -106,7 +147,7 @@ done
 echo -e "########################### Generating Script ############################# \n"
 
 # Save script to user's home directory as a hidden file
-OUTPUT_SCRIPT="$HOME/.backup_script.sh"
+OUTPUT_SCRIPT="$BACKUP_DIR/.backup_script.sh"
 
 cat <<EOL > "$OUTPUT_SCRIPT"
 #!/bin/bash
@@ -138,12 +179,12 @@ if [ -e "\$WEEKLY_TIMESTAMP_FILE" ]; then
 
     # If it's been less than 7 days since the last weekly backup, skip the full backup
     if [ "\$WEEKLY_TIME_DIFF" -lt \$((7 * 86400)) ]; then
-        echo "Weekly backup has already been done within the last 7 days. Skipping weekly backup."
-        echo ""
+        echo -e "Weekly backup has already been done within the last 7 days. Skipping weekly backup.\\n"
     else
         # Do the weekly full backup
-        echo "Running weekly full backup."
-        echo "DO NOT close the terminal until complete. This may take a while depending on size of backup."
+        echo -e "Running weekly full backup.\\nDO NOT close the terminal until complete.\\nThis may take a while depending on size of backup.\\n"
+
+        # Copy and compress backup to designated folder
         tar -cvpzf "\$FULL_BACKUP_DIR/full_backup_\$DATE.tar.gz" \\
             --exclude-from="\$EXCLUDE_LIST" \\
             --listed-incremental="\$SNAPSHOT_FILE" \\
@@ -156,20 +197,29 @@ if [ -e "\$WEEKLY_TIMESTAMP_FILE" ]; then
 
         # Update weekly backup timestamp
         echo "\$TIME" > "\$WEEKLY_TIMESTAMP_FILE"
+
+        # Notify user of completion
+        echo -e "Backup Complete! \\n"
+
     fi
 else
     # If the weekly backup timestamp file doesn't exist, do the first full backup
-    echo "First weekly backup. Running full backup."
-    echo "DO NOT close the terminal until complete. This may take a while depending on size of backup."
-    echo ""
+    echo -e "Running first initial backup.\\nDO NOT close the terminal until complete. This may take a while depending on size of backup.\\n"
+
+    # Copy and compress backup to designated folder
     tar -cvpzf "\$FULL_BACKUP_DIR/full_backup_\$DATE.tar.gz" \\
         --exclude-from="\$EXCLUDE_LIST" \\
         --listed-incremental="\$SNAPSHOT_FILE" \\
         --totals \\
         \$BACKUP_TARGET &> "\$FULL_BACKUP_DIR/full_backup_\$DATE.log"
 
-    # Update weekly backup timestamp
+    # Update weekly backup timestamp and define WEEKYL_TIME_DIFF for daily backup.
     echo "\$TIME" > "\$WEEKLY_TIMESTAMP_FILE"
+    LAST_WEEKLY_RUN_TIME=\$(cat "\$WEEKLY_TIMESTAMP_FILE")
+    WEEKLY_TIME_DIFF=\$((\$TIME - \$LAST_WEEKLY_RUN_TIME))
+
+    # Notify user of completion
+    echo -e "Backup Complete!\\n"
 fi
 
 # Now proceed with the daily backup logic using a similar timestamp check
@@ -178,36 +228,36 @@ if [ -e "\$DAILY_TIMESTAMP_FILE" ]; then
     DAILY_TIME_DIFF=\$((TIME - LAST_DAILY_RUN_TIME))
 
     # If it's been less than 24 hours since the last daily backup, skip
-    if [ "\$DAILY_TIME_DIFF" -lt 86400 ]; then
-        echo -e "Daily backup has already been done within the last 24 hours. Skipping daily backup."
-        echo ""
-        exit 0
+    if [ "\$DAILY_TIME_DIFF" -lt 86400 ] && [ "\$WEEKLY_TIME_DIFF" -lt 86400 ]; then
+        echo -e "A backup has already been done within the last 24 hours. Skipping daily backup.\\n\\nExiting Script.\\n"
+    else
+        # Perform daily incremental backup (if timestamp check passes)
+        echo -e "Running daily incremental backup.\\nDO NOT close the terminal until complete. This may take a while depending on size of backup.\\n"
+
+        # Copy and compress backup to designated folder
+        tar -cvpzf "\$DAILY_BACKUP_DIR/incremental_backup_\$DATE.tar.gz" \\
+            --listed-incremental="\$SNAPSHOT_FILE" \\
+            --exclude-from="\$EXCLUDE_LIST" \\
+            --totals \\
+            \$BACKUP_TARGET &> "\$DAILY_BACKUP_DIR/incremental_backup_\$DATE.log"
+
+        # Remove older daily backups if there are more than the daily limit
+        find "\$DAILY_BACKUP_DIR" -name "incremental_backup_*.tar.gz" -type f | sort -r | tail -n +\$DAILY_REMOVE  | xargs rm -f
+        find "\$DAILY_BACKUP_DIR" -name "incremental_backup_*.log" -type f | sort -r | tail -n +\$DAILY_REMOVE | xargs rm -f
+
+        # Notify user of completion
+        echo -e "Backup Complete! Exiting Script. \\n"
     fi
+else
+    # If the daily backup timestamp doesnt exist, create one using the weekly backup timestamp
+    echo "\$TIME" > "\$DAILY_TIMESTAMP_FILE"
 fi
-
-    # Perform daily incremental backup (if timestamp check passes)
-    echo "Running daily incremental backup."
-    echo "DO NOT close the terminal until complete. This may take a while depending on size of backup."
-    echo ""
-    tar -cvpzf "\$DAILY_BACKUP_DIR/incremental_backup_\$DATE.tar.gz" \\
-        --listed-incremental="\$SNAPSHOT_FILE" \\
-        --exclude-from="\$EXCLUDE_LIST" \\
-        --totals \\
-        \$BACKUP_TARGET &> "\$DAILY_BACKUP_DIR/incremental_backup_\$DATE.log"
-
-    # Remove older daily backups if there are more than the daily limit
-    find "\$DAILY_BACKUP_DIR" -name "incremental_backup_*.tar.gz" -type f | sort -r | tail -n +\$DAILY_REMOVE  | xargs rm -f
-    find "\$DAILY_BACKUP_DIR" -name "incremental_backup_*.log" -type f | sort -r | tail -n +\$DAILY_REMOVE | xargs rm -f
-
-# Update the daily backup timestamp
-echo "\$TIME" > "\$DAILY_TIMESTAMP_FILE"
 EOL
 
 # Make generated script executable
 chmod +x "$OUTPUT_SCRIPT"
 
-echo "Script created at "$OUTPUT_SCRIPT" with the provided settings."
-echo ""
+echo -e "Script created at "$OUTPUT_SCRIPT" with the provided settings.\n"
 
 # Run the generated script
 $OUTPUT_SCRIPT
@@ -218,11 +268,13 @@ mkdir -p "$HOME/.config/autostart"
 # Set location of desktop file to be created
 AUTOSTART="$HOME/.config/autostart/backup_script.sh.desktop"
 
+echo -e "####################### Generating Autostart Entry ######################### \n"
+
 # create a desktop entry within the autostart folder to run when user logs in.
 cat <<EOL > "$AUTOSTART"
 [Desktop Entry]
 Type=Application
-Exec=$HOME/.backup_script.sh
+Exec=$OUTPUT_SCRIPT
 Icon=application-x-shellscript
 Name=backup.sh
 Comment=Runs the backup script created with Blair's Simple Backup
@@ -230,7 +282,9 @@ StartupNotify=true
 Terminal=false
 EOL
 
+# Show user autostart was created
 echo -e "Autostart entry created at $AUTOSTART \n"
 
 #Ending Script
-echo -e "Backup completed! Please check the log files to ensure everything was properly archived. \n"
+echo -e "############################ Backup Complete! ##############################\n"
+echo -e "Please check the log files to ensure everything was properly archived.\n"
