4c24080c78baed283a83024b61a308163c06a4be
John R. Blair <johnrossblair@gmail.com>
2024-11-18 23:41:33 -0600

    added functions and timers. fixed duplicated code.

--- simple_backup_script.sh
+++ simple_backup_script.sh
@@ -23,155 +23,177 @@ echo "#                                                            #"
 echo "##############################################################"
 echo ""
 
+# Functions
+
+clean_path() {
+    if [[ "${ANSWER:0:1}" == "~" ]]; then   # Replace "~" with "home/$USER" and remove "/" at the end if either exists.
+        ANSWER="/home/$USER${ANSWER:1}"     # This resolves the issue where if user uses "~" to specify their home directory,
+    fi                                      # it will replace it with the full path so that the path can be checked if it exists or not.
+
+    if [[ "${ANSWER: -1}" == "/" ]]; then   # Removing the "/" will prevent the script from adding "//" when specifiying
+        ANSWER="${ANSWER%/}"                # where the "/backups" will be located.
+    fi
+}
+
+is_valid_number() {     # Verify that the answer is greater than 0, and a valid int.
+    while true; do
+        if [[ "$ANSWER" =~ ^[0-9]+$ ]] && [ "$ANSWER" -gt 0 ]; then             
+            echo -e "$ANSWER backup(s) will be stored at one time. \n"
+            sleep 1 # Give the user time to read the output before moving on.
+            break
+        else
+            read -ep "'$ANSWER' is not a valid answer. Please try again: " ANSWER
+            echo""
+        fi
+    done
+}   
+
+is_y_n() {      #Verify that the answer is either 'y' or 'n'.
+    while true; do
+        if [ "$ANSWER" == "y" ] || [ "$ANSWER" == "n" ]; then
+            break
+        else
+            read -ep "Invalid reponse. Please type 'y' or 'n': " ANSWER
+            echo ""
+        fi
+    done
+}
+
 # Choose backup directory
-read -ep "Where would you like to save your backups? (ex: /path/to/backups): " BACKUP_DIR
+read -ep "Where would you like to save your backups? (ex: /path/to/backups): " ANSWER
 echo ""
 
-    # Clean up path by removing "/" at end if submitted, and also generating full path if user uses ~ shortcut for home
-    if [[ "${BACKUP_DIR:0:1}" == "~" ]]; then
-        BACKUP_DIR="/home/$USER${BACKUP_DIR:1}"
-    fi
-
-    if [[ "${BACKUP_DIR: -1}" == "/" ]]; then
-        BACKUP_DIR="${BACKUP_DIR%/}"
-    fi
+    # Call function to clean path and set backup 
+    clean_path
+    BACKUP_DIR=$ANSWER
 
     # Loop until the entered path exists
     while [ ! -e "$BACKUP_DIR" ]; do
         echo -e "The path '$BACKUP_DIR' does not exist. Please try again. \n"
-        read -ep "Where would you like to save your backups? (ex: /path/to/backups): " BACKUP_DIR
+        sleep 1 # Give the user time to read the output before moving on.
+        read -ep "Where would you like to save your backups? (ex: /path/to/backups): " ANSWER
         echo ""
+        
+        # Call function to clean path and set backup 
+        clean_path
+        BACKUP_DIR=$ANSWER
     done
 
-BACKUP_DIR="${BACKUP_DIR}/backups"
+    BACKUP_DIR="${BACKUP_DIR}/backups"  # Place folder within the directory called "backups"
+    mkdir -p $BACKUP_DIR                # Make directory if it does not already exist
 
-mkdir -p $BACKUP_DIR
+echo -e "Backups will be stored in: $BACKUP_DIR \n"    # Echo the location of backups
+sleep 1 # Give the user time to read the output before moving on
 
 # Choose directory to back up
-read -ep "What folder would you like to backup? (ex: /home/yourname/Pictures): " BACKUP_TARGET
+read -ep "What folder would you like to backup? (ex: /home/yourname/Pictures): " ANSWER
 echo ""
 
-    # Clean up path by removing "/" at end if submitted, and also generating full path if user uses ~ shortcut for home
-    if [[ "${BACKUP_TARGET:0:1}" == "~" ]]; then
-        BACKUP_TARGET="/home/$USER${BACKUP_TARGET:1}"
-    fi
-
-    if [[ "${BACKUP_TARGET: -1}" == "/" ]]; then
-        BACKUP_TARGET="${BACKUP_TARGET%/}"
-    fi
+    # Call function to clean path
+    clean_path
+    BACKUP_TARGET=$ANSWER
 
     # Loop until the entered path exists
     while [ ! -e "$BACKUP_TARGET" ]; do
         echo -e "The path '$BACKUP_TARGET' does not exist. Please try again. \n"
-        read -ep "What folder would you like to backup? (ex: /home/yourname/Pictures): " BACKUP_TARGET
+        sleep 1 # Give the user time to read the output before moving on.
+        read -ep "What folder would you like to backup? (ex: /home/yourname/Pictures): " ANSWER
         echo ""
+
+        # Call function to clean path and set backup 
+        clean_path
+        BACKUP_TARGET=$ANSWER
     done
 
-# Path to exclusion list
-EXCLUDE_LIST="$BACKUP_DIR/.exclude_list.txt"
+echo -e "$BACKUP_TARGET selected.\n"
+sleep 1 # Give the user time to read the output before moving on.
 
-# Ensure list exsists
+# Path to exclusion list/ensure list exists
+EXCLUDE_LIST="$BACKUP_DIR/.exclude_list.txt"
 touch "$EXCLUDE_LIST"
 
-# add backup directory to avoid backing up a backup - failsafe
+# Add backup directory to avoid backing up a backup - failsafe
 echo "$BACKUP_DIR" > "$EXCLUDE_LIST"
 
-# verify that answer is valid before proceeding
-while true; do
-    read -p "Would you like to exclude all hidden files and folders within the directory being backed up? (y/n): " EXCLUDE_HIDDEN_ANSWER
-    echo ""
-    if [ "$EXCLUDE_HIDDEN_ANSWER" == "y" ] || [ "$EXCLUDE_HIDDEN_ANSWER" == "n" ]; then
-        break
-    else
-        echo -e "Invalid reponse. Please type 'y' or 'n'.\n"
-    fi
-done
+read -p "Would you like to exclude all hidden files and folders within the directory being backed up? (y/n): " ANSWER
+echo ""
 
-# Give option to exclude all hidden files and folders
-if [ "$EXCLUDE_HIDDEN_ANSWER" == "y" ]; then
-    echo "$BACKUP_TARGET/.*" >> $EXCLUDE_LIST
-    echo -e "All hidden files and folders within directory will be ignored.\n"
-else
-    echo -e "The backup will include all hidden files and folders.\n"
-fi
+    # Verify answer is valid & set the variable
+    is_y_n
+    EXCLUDE_HIDDEN_ANSWER=$ANSWER
 
-# verify that answer is valid before proceeding
-while true; do
-    read -p "Are there any other folders or files you would like to exclude within the folder? (y/n): " EXCLUDE_ANSWER
-    echo ""
-    if [ "$EXCLUDE_ANSWER" == "y" ] || [ "$EXCLUDE_ANSWER" == "n" ]; then
-        break
+    # Give option to exclude all hidden files and folders
+    if [ "$EXCLUDE_HIDDEN_ANSWER" == "y" ]; then
+        echo "$BACKUP_TARGET/.*" >> $EXCLUDE_LIST
+        echo -e "All hidden files and folders within directory will be ignored.\n"
     else
-        echo -e "Invalid reponse. Please type 'y' or 'n'.\n"
+        echo -e "The backup will include all hidden files and folders.\n"
     fi
-done
+
+    sleep 1 # Give the user time to read the output before moving on.
+
+read -p "Are there any other folders or files you would like to exclude within the folder? (y/n): " ANSWER
+echo ""
+
+# Verify answer is valid & set the variable
+is_y_n
+EXCLUDE_ANSWER=$ANSWER
 
 # Check for file/folder exclusion within the directory to be archived
 if [ "$EXCLUDE_ANSWER" == "y" ]; then
     # If yes, enter the loop to collect exclusions
     while true; do
         # Ask for a path to exclude
-        read -ep "Enter a path to exclude from the backup (or type 'done' to finish): " EXCLUDE_PATH
+        read -ep "Enter a path to exclude from the backup (or type 'done' to finish): " ANSWER
         echo ""
-
-        # Clean up path by removing "/" at end if submitted, and also generating full path if user uses ~ shortcut for home
-        if [[ "${EXCLUDE_PATH:0:1}" == "~" ]]; then
-            EXCLUDE_PATH="/home/$USER${EXCLUDE_PATH:1}"
-        fi
-
-        if [[ "${EXCLUDE_PATH: -1}" == "/" ]]; then
-            EXCLUDE_PATH="${EXCLUDE_PATH%/}"
-        fi
+        # Call function to clean path
+        clean_path
+        # Set the variable after function
+        EXCLUDE_PATH=$ANSWER
 
         # Check if the user typed 'done' to exit the loop
         if [ "$EXCLUDE_PATH" == "done" ]; then
             echo "Exclusion list complete. The following paths will not be backed up:"
             echo "$(cat $EXCLUDE_LIST)"
             echo ""
+            sleep 1 # Give the user time to read the output before moving on.
             break
         fi
 
-        # Validate if the path exists
+        # Add to list & validate if the path exists
         if [ -e "$EXCLUDE_PATH" ]; then
             # Append the valid path to the exclusion list
             echo "$EXCLUDE_PATH" >> "$EXCLUDE_LIST"
             echo -e "Added '$EXCLUDE_PATH' to the exclusion list. \n"
+            sleep 1 # Give the user time to read the output before moving on.
         else
             echo -e "The path '$EXCLUDE_PATH' does not exist. Please try again. \n"
+            sleep 1 # Give the user time to read the output before moving on.
         fi
     done
 else
     echo -e "No exclusions will be added. \n"
+    sleep 1 # Give the user time to read the output before moving on.
 fi
 
 # Loop for weekly backups
-while true; do
-    #Ask how many weekly backups to store
-    read -p "How many weekly backups would you like to keep?: " WEEKLY_LIMIT
-    echo ""
-    if [[ "$WEEKLY_LIMIT" =~ ^[0-9]+$ ]] && [ "$WEEKLY_LIMIT" -gt 0 ]; then
-        echo -e "$WEEKLY_LIMIT weekly backups will be stored at one time. \n"
-        break
-    else
-        echo -e "'$WEEKLY_LIMIT' is not a valid answer. Please try again. \n"
-    fi
-done
+read -p "How many weekly backups would you like to keep?: " ANSWER
+echo ""
+# Call function to check if answer is valid
+is_valid_number
+# Set the variable after function
+WEEKLY_LIMIT=$ANSWER
 
 # Loop for daily backups
-while true; do
-    #Ask how many daily backups to store
-    read -p "How many daily incremental backups would you like to keep?: " DAILY_LIMIT
-    echo ""
-    if [[ "$DAILY_LIMIT" =~ ^[0-9]+$ ]] && [ "$DAILY_LIMIT" -gt 0 ]; then
-        echo -e "$DAILY_LIMIT daily backups will be stored at one time. \n"
-        break
-    else
-        echo -e "'$DAILY_LIMIT' is not a valid answer. Please try again. \n"
-    fi
-done
+read -p "How many daily incremental backups would you like to keep?: " ANSWER
+echo ""
+# Call function to check if answer is valid
+is_valid_number
+# Set the variable after function
+DAILY_LIMIT=$ANSWER
 
-#Generate the script
-echo -e "########################### Generating Script ############################# \n"
+# Generate the script
+echo "########################### Generating Script #############################"
 
 # Save script to user's home directory as a hidden file
 OUTPUT_SCRIPT="$BACKUP_DIR/.backup_script.sh"
@@ -207,6 +229,7 @@ if [ -e "\$WEEKLY_TIMESTAMP_FILE" ]; then
     # If it's been less than 7 days since the last weekly backup, skip the full backup
     if [ "\$WEEKLY_TIME_DIFF" -lt \$((7 * 86400)) ]; then
         echo -e "Weekly backup has already been done within the last 7 days. Skipping weekly backup.\\n"
+        sleep 1 # Give the user time to read the output before moving on.
     else
         # Do the weekly full backup
         echo -e "Running weekly full backup.\\nDO NOT close the terminal until complete.\\nThis may take a while depending on size of backup.\\n"
@@ -232,6 +255,7 @@ if [ -e "\$WEEKLY_TIMESTAMP_FILE" ]; then
 
         # Notify user of completion
         echo -e "Backup Complete! \\n"
+        sleep 1 # Give the user time to read the output before moving on.
 
     fi
 else
@@ -252,6 +276,7 @@ else
 
     # Notify user of completion
     echo -e "Backup Complete!\\n"
+    sleep 1 # Give the user time to read the output before moving on.
 fi
 
 # Now proceed with the daily backup logic using a similar timestamp check
@@ -262,6 +287,7 @@ if [ -e "\$DAILY_TIMESTAMP_FILE" ]; then
     # If it's been less than 24 hours since the last daily backup, skip
     if [ "\$DAILY_TIME_DIFF" -lt 86400 ] || [ "\$WEEKLY_TIME_DIFF" -lt 86400 ]; then
         echo -e "A backup has already been done within the last 24 hours. Skipping daily backup.\\n\\nExiting Script.\\n"
+        sleep 1 # Give the user time to read the output before moving on.
     else
         # Perform daily incremental backup (if timestamp check passes)
         echo -e "Running daily incremental backup.\\nDO NOT close the terminal until complete. This may take a while depending on size of backup.\\n"
@@ -281,6 +307,7 @@ if [ -e "\$DAILY_TIMESTAMP_FILE" ]; then
 
         # Notify user of completion
         echo -e "Backup Complete! Exiting Script. \\n"
+        sleep 1 # Give the user time to read the output before moving on.
     fi
 else
     # If the daily backup timestamp doesnt exist, create one using the weekly backup timestamp
@@ -288,23 +315,22 @@ else
 fi
 EOL
 
-# Make generated script executable
+# Modify the script to make it executable, then run the generated script
 chmod +x "$OUTPUT_SCRIPT"
-
+sleep 1 # Give the user time to read the output before moving on.
 echo -e "Script created at "$OUTPUT_SCRIPT" with the provided settings.\n"
-
-# Run the generated script
+sleep 1 # Give the user time to read the output before moving on.
 $OUTPUT_SCRIPT
 
 # Verify the directory exists
 mkdir -p "$HOME/.config/autostart"
 
-# Set location of desktop file to be created
+# Set location where the desktop file will be created
 AUTOSTART="$HOME/.config/autostart/backup_script.sh.desktop"
 
-echo -e "####################### Generating Autostart Entry ######################### \n"
+echo "####################### Generating Autostart Entry #########################"
 
-# create a desktop entry within the autostart folder to run when user logs in.
+# Create a desktop entry within the autostart folder to run when user logs in.
 cat <<EOL > "$AUTOSTART"
 [Desktop Entry]
 Type=Application
@@ -317,8 +343,10 @@ Terminal=false
 EOL
 
 # Show user autostart was created
+sleep 1 # Give the user time to read the output before moving on.
 echo -e "Autostart entry created at $AUTOSTART \n"
+sleep 1 # Give the user time to read the output before moving on.
 
-#Ending Script
-echo -e "############################ Backup Complete! ##############################\n"
+# Ending Script
+echo "############################ Backup Complete! ##############################"
 echo -e "Please check the log files to ensure everything was properly archived.\n"
